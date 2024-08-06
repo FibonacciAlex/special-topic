a8a1fef0c992cd9cd2d9ae4e8adc46484a247c16
FibonacciAlex <sleepagain123@163.com>
2024-08-06 19:39:01 +1200

    Change Runing logic of Task8

--- Main.java
+++ Main.java
@@ -14,7 +14,10 @@ public class Main {
     protected static Map<String, Lecturer> LECTURES = new HashMap<>();
 
     //lecture and course data, key=LectureID,value=<DeliverType, course>
-    protected static Map<String, Map<DeliverType,Course>> courseMatchData = new HashMap<>();
+    protected static Map<String, Map<Campus,Course>> courseMatchData = new HashMap<>();
+
+
+    protected static Map<CampusMatchKey, Lecturer> lecturerMatchData = new HashMap<>();
 
     // Initial data
     static {
@@ -33,16 +36,16 @@ public class Main {
         COURSES.add(new Course(
                 "158345","Professionalism in the Information Sciences","IT, IS, CS, DS, SE",100,0,0));
 
-        LECTURES.put("1105236", new Lecturer("1105236","Amy","Sheffield","PN      "));
-        LECTURES.put("1235894", new Lecturer("1235894","Victoria","Jensen","PN      "));
-        LECTURES.put("7225669", new Lecturer("7225669","James","Lee","PN      "));
-        LECTURES.put("1328991", new Lecturer("1328991","Colin","Delmont","PN      "));
-        LECTURES.put("1562347", new Lecturer("1562347","Thomas","Becker","Auckland"));
-        LECTURES.put("5664789", new Lecturer("5664789","Steven","Hobbs","Auckland"));
-        LECTURES.put("3658947", new Lecturer("3658947","Andrew","Jackson","Auckland"));
-        LECTURES.put("6332698", new Lecturer("6332698","Jonathon","Wood","Auckland"));
-        LECTURES.put("12345678", new Lecturer("12345678","Mickey","Mouse","Auckland"));
-        LECTURES.put("24013122", new Lecturer("24013122","Shance","Zhao","Auckland"));
+        LECTURES.put("1105236", new Lecturer("1105236","Amy","Sheffield",Campus.PN));
+        LECTURES.put("1235894", new Lecturer("1235894","Victoria","Jensen",Campus.PN));
+        LECTURES.put("7225669", new Lecturer("7225669","James","Lee",Campus.PN));
+        LECTURES.put("1328991", new Lecturer("1328991","Colin","Delmont",Campus.PN));
+        LECTURES.put("1562347", new Lecturer("1562347","Thomas","Becker",Campus.AUCKLAND));
+        LECTURES.put("5664789", new Lecturer("5664789","Steven","Hobbs",Campus.AUCKLAND));
+        LECTURES.put("3658947", new Lecturer("3658947","Andrew","Jackson",Campus.AUCKLAND));
+        LECTURES.put("6332698", new Lecturer("6332698","Jonathon","Wood",Campus.AUCKLAND));
+        LECTURES.put("12345678", new Lecturer("12345678","Mickey","Mouse",Campus.AUCKLAND));
+        LECTURES.put("24013122", new Lecturer("24013122","Shance","Zhao",Campus.AUCKLAND));
     }
     public static void main(String[] args){
         // System init
@@ -55,13 +58,13 @@ public class Main {
     public static List<AbsTask> initSystem(){
         List<AbsTask> tasks = new ArrayList<>();
         tasks.add(new Task1("School Full Name: School of Mathematical and Computational Sciences"));
-        tasks.add(new Task2("All papers details"));
-        tasks.add(new Task3(Major.CS, "Papers that belong to Major CS"));
-        tasks.add(new Task4("Paper that have exam"));
-        tasks.add(new Task5("Papers that their assignments weight higher than 50.0%"));
-        tasks.add(new Task6("Paper offering details"));
-        tasks.add(new Task7("24013122","The paper offering that I teach"));
-        tasks.add(new Task8("159234","The lecturer of Auckland offering of 159234"));
+        tasks.add(new Task2("All papers details:"));
+        tasks.add(new Task3(Major.CS, "Papers that belong to Major CS:"));
+        tasks.add(new Task4("Paper that have exam:"));
+        tasks.add(new Task5("Papers that their assignments weight higher than 50.0%:"));
+        tasks.add(new Task6("Paper offering details:"));
+        tasks.add(new Task7("24013122","The paper offering that I teach:"));
+        tasks.add(new Task8("159234",Campus.AUCKLAND, "The lecturer of Auckland offering of 159234:"));
         return tasks;
     }
 
@@ -164,9 +167,21 @@ enum Major{
 /**
  * Course Deliver Campus
  */
-enum DeliverType{
-    DISTANCE,
-    CAMPUS;
+enum Campus{
+    DISTANCE("Distance"),
+    AUCKLAND("Auckland"),
+    PN      ("PN      ");
+
+    private String campusName;
+
+    public String getCampusName() {
+        return campusName;
+    }
+
+    Campus(String name) {
+        campusName = name;
+    }
+
 }
 
 /**
@@ -178,9 +193,9 @@ class Lecturer{
 
     private String firstName;
     private String lastName;
-    private String campus;
+    private Campus campus;
 
-    public Lecturer(String id, String firstName, String lastName, String campus) {
+    public Lecturer(String id, String firstName, String lastName, Campus campus) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -196,9 +211,53 @@ class Lecturer{
         return firstName+" "+lastName;
     }
 
-    public String getCampus() {
+    public Campus getCampus() {
         return campus;
     }
+}
+
+/**
+ * Course and Campus match Key
+ */
+class CampusMatchKey{
+
+    private String courseNumber;
+
+    private Campus campus;
+
+    public CampusMatchKey(String courseNumber, Campus campus) {
+        this.courseNumber = courseNumber;
+        this.campus = campus;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((courseNumber == null) ? 0 : courseNumber.hashCode());
+        result = prime * result + ((campus == null) ? 0 : campus.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CampusMatchKey other = (CampusMatchKey) obj;
+        if (courseNumber == null) {
+            if (other.courseNumber != null)
+                return false;
+        } else if (!courseNumber.equals(other.courseNumber))
+            return false;
+        if (campus != other.campus)
+            return false;
+        return true;
+    }
+
 }
 
 /**
@@ -215,7 +274,7 @@ abstract class AbsTask{
     }
     protected void info(){
         System.out.println("-------------- Task "+id+"---------------------");
-        System.out.println(taskTitle+":");
+        System.out.println(taskTitle);
     }
     abstract void run();
 }
@@ -315,33 +374,32 @@ class Task6 extends AbsTask{
             List<String> lecturers = Main.LECTURES.keySet().stream().collect(Collectors.toList());
 
             Random random = new Random();
-            for (DeliverType deliverType : DeliverType.values()) {
+            for (Campus deliverType : Campus.values()) {
 
                 while (true) {
                     int index = random.nextInt(lecturers.size());
                     String lid = lecturers.get(index);
-                    //check course number of this lecture
-                    Map<DeliverType, Course> assignCourses = Main.courseMatchData.get(lid);
-                    if (assignCourses == null) {
-                        assignCourses = new HashMap<>();
+                    //check how many courses of this lecturer teaching now
+                    Map<Campus, Course> courseofLecturer = Main.courseMatchData.get(lid);
+                    if (courseofLecturer == null) {
+                        courseofLecturer = new HashMap<>();
                     }
-                    //check this lecture in campus course
-                    if (deliverType.equals(DeliverType.CAMPUS) && assignCourses.get(deliverType) != null) {
+                    //check if this is in campus course and match the lecturer's campus
+                    Lecturer lecturer = Main.LECTURES.get(lid);
+                    if (!deliverType.equals(Campus.DISTANCE) && !lecturer.getCampus().equals(deliverType)) {
                         //random again
                         continue;
                     }
-                    assignCourses.put(deliverType, i);
-                    Main.courseMatchData.put(lid, assignCourses);
-                    Lecturer lecturer = Main.LECTURES.get(lid);
-                    String dType = "Distance";
-                    if(deliverType.equals(DeliverType.CAMPUS)){
-                        dType = lecturer.getCampus();
-                    }
-                    System.out.println(i.getNumber()+" "+ dType + " "+lecturer.getName());
+                    courseofLecturer.put(deliverType, i);
 
-                    if (assignCourses.size() >= 4) {
+                    //save matching data
+                    Main.courseMatchData.put(lid, courseofLecturer);
+                    Main.lecturerMatchData.put(new CampusMatchKey(i.getNumber(), deliverType), lecturer);
+                    
+                    System.out.println(i.getNumber()+" "+ deliverType.getCampusName() + " "+lecturer.getName());
+
+                    if (courseofLecturer.size() >= 4) {
                         //this lecturer can not accept courses anymore
-
                         lecturers.remove(index);
                         System.out.println("----------remove lecturer id+"+lid+"----index:"+index+"--remain length+"+lecturers.size());
                     }
@@ -365,15 +423,11 @@ class Task7 extends AbsTask{
     @Override
     void run() {
         info();
-        Map<DeliverType, Course> map = Main.courseMatchData.get(myID);
+        Map<Campus, Course> map = Main.courseMatchData.get(myID);
         Lecturer lecturer = Main.LECTURES.get(myID);
         if(map!= null){
             map.forEach((deliverType, course) -> {
-                String dType = "Distance";
-                if(DeliverType.CAMPUS.equals(deliverType)){
-                    dType = lecturer.getCampus();
-                }
-                System.out.println("Paper Offering -  " + course.getCourseName() + "    "+ dType + "    Lecturer: " + lecturer.getName());
+                System.out.println("Paper Offering -  " + course.getCourseName() + "    "+ deliverType.getCampusName() + "    Lecturer: " + lecturer.getName());
             });
             System.out.println("I am teaching "+ map.size()+" paper(s)");
         }
@@ -381,40 +435,24 @@ class Task7 extends AbsTask{
 }
 
 class Task8 extends AbsTask{
+
     private String courseNumber;
-    Task8(String courseId, String title) {
+
+    private Campus campus;
+
+    Task8(String courseId,Campus campus, String title) {
         super(8, title);
         this.courseNumber = courseId;
+        this.campus = campus;
     }
 
     @Override
     void run() {
         info();
         //use target courseNumber to find in campus teaching lecturer
-        String targetLecturerId = "";
-        T1:for (String i : Main.courseMatchData.keySet()) {
-            Map<DeliverType, Course> map = Main.courseMatchData.get(i);
-            for (Map.Entry<DeliverType, Course> entry : map.entrySet()) {
-                DeliverType k = entry.getKey();
-                Course v = entry.getValue();
-                if (v.getNumber().equals(courseNumber) && DeliverType.CAMPUS.equals(k)) {
-                    targetLecturerId = i;
-                    break T1;
-                }
-            }
-        };
-
-        if(targetLecturerId == ""){
-            System.out.println("No lecturer teach "+ courseNumber+ " in campus!");
-            return;
-        }
-        Lecturer lecturer = Main.LECTURES.get(targetLecturerId);
-        if(!lecturer.getCampus().equals("Auckland")){
-            System.out.println("lecturer "+lecturer.getName()+" teach "+ courseNumber+ "in campus "+ lecturer.getCampus());
-            return;
-        }
-        System.out.println("Lecturer's Name:" + lecturer.getName());
-        Map<DeliverType, Course> map = Main.courseMatchData.get(lecturer.getId());
+        Lecturer lecturer = Main.lecturerMatchData.get(new CampusMatchKey(courseNumber, campus));
+        System.out.println("Lecturer's Name: " + lecturer.getName());
+        Map<Campus, Course> map = Main.courseMatchData.get(lecturer.getId());
         System.out.println("This lecturer is teaching "+ map.keySet().size()+" paper(s)");
     }
 }
